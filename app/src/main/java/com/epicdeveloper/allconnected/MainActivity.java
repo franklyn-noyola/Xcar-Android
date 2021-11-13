1bd17900c1a97dabfe9dbf2719c8b342d0935573
franklyn-noyola <franklyn.noyola@gmail.com>
2021-11-13 10:25:36 +0100

    updated

--- app/src/main/java/com/epicdeveloper/allconnected/MainActivity.java
+++ app/src/main/java/com/epicdeveloper/allconnected/MainActivity.java
@@ -487,15 +487,7 @@ public class MainActivity extends AppCompatActivity {
                         if (!MainActivity.getInstance(getApplication()).isOnline(getApplication())) {
                             Toast.makeText(getApplication(), resources.getString(R.string.noConexion), Toast.LENGTH_LONG).show();
                         }
-                        /*if (fromNotifications == 1) {
-                            Intent intent = new Intent(getApplicationContext(), fragment_chat.class);
-                            startActivity(intent);
 
-                        }*/ else if(fromNotifications == 2) {
-
-                            Intent intent = new Intent(getApplicationContext(), viewNotification.class);
-                            startActivity(intent);
-                        } else {
                         if (init == 1 && profileView == 1) {
                             finish();
                         }
@@ -551,7 +543,7 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             });
                         }
-                      }
+
                     }
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
