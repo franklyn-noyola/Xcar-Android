401a6870ab592cdd08334383f81c27bf9537eed7
franklyn-noyola <franklyn.noyola@gmail.com>
2021-12-03 13:48:36 +0100

    Updated

--- app/src/main/java/com/epicdeveloper/allconnected/contacto.java
+++ app/src/main/java/com/epicdeveloper/allconnected/contacto.java
@@ -7,19 +7,14 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.epicdeveloper.allconnected.ui.sendNotifications.sendNotification;
-
 import static com.epicdeveloper.allconnected.sendEmail.sendEmailMessage;
+import java.util.Objects;
 
 public class contacto extends AppCompatActivity {
     Spinner selectSubject;
@@ -36,32 +31,27 @@ public class contacto extends AppCompatActivity {
         selectedLanguage = MainActivity.userlanguage;
         context = LocaleHelper.setLocale(getApplication(), selectedLanguage);
         resources = context.getResources();
-        selectSubject = (Spinner) findViewById(R.id.subject);
-        message = (EditText) findViewById(R.id.contactMessage);
+        selectSubject = findViewById(R.id.subject);
+        message = findViewById(R.id.contactMessage);
 
-        sendMessage= (TextView) findViewById(R.id.buttonSend);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0099CC")));
+        sendMessage= findViewById(R.id.buttonSend);
+        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0099CC")));
         getSupportActionBar().setTitle(resources.getString(R.string.action_contacts));
-        selectSubject.setAdapter(new ArrayAdapter<String>(contacto.this, android.R.layout.simple_expandable_list_item_1,resources.getStringArray(R.array.contactos_asuntos)));
+        selectSubject.setAdapter(new ArrayAdapter<>(contacto.this, android.R.layout.simple_expandable_list_item_1,resources.getStringArray(R.array.contactos_asuntos)));
         translatedLanguage();
-        sendMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        sendMessage.setOnClickListener(v -> {
 
-                if (TextUtils.equals(resources.getString(R.string.issueSelect),selectSubject.getSelectedItem().toString())){
-                    Toast.makeText(getApplicationContext(), resources.getString(R.string.issueEmpty), Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                else if (TextUtils.isEmpty(message.getText().toString())){
-                    Toast.makeText(getApplicationContext(), resources.getString(R.string.emptyMessage), Toast.LENGTH_SHORT).show();
-                    return;
-                }else{
-                    sendEmailMessage("allconnected_support@epicdevelopers.app",MainActivity.plateUser+"-"+selectSubject.getSelectedItem().toString(), message.getText().toString());
-                    Toast.makeText(getApplicationContext(), resources.getString(R.string.sentMessage), Toast.LENGTH_SHORT).show();
-                    finish();
-                    Intent intent = new Intent(getApplicationContext(), profile_activity.class);
-                    startActivity(intent);
-                }
+            if (TextUtils.equals(resources.getString(R.string.issueSelect),selectSubject.getSelectedItem().toString())){
+                Toast.makeText(getApplicationContext(), resources.getString(R.string.issueEmpty), Toast.LENGTH_SHORT).show();
+            }
+            else if (TextUtils.isEmpty(message.getText().toString())){
+                Toast.makeText(getApplicationContext(), resources.getString(R.string.emptyMessage), Toast.LENGTH_SHORT).show();
+            }else{
+                sendEmailMessage("allconnected_support@epicdevelopers.app",MainActivity.plateUser+"-"+selectSubject.getSelectedItem().toString(), message.getText().toString());
+                Toast.makeText(getApplicationContext(), resources.getString(R.string.sentMessage), Toast.LENGTH_SHORT).show();
+                finish();
+                Intent intent = new Intent(getApplicationContext(), profile_activity.class);
+                startActivity(intent);
             }
         });
 
