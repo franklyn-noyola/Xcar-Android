c8cc2ab6cea79f616d0c89c0ee6b7ff602f7e6eb
franklyn-noyola <franklyn.noyola@gmail.com>
2021-12-03 13:46:18 +0100

    Updated

--- app/src/main/java/com/epicdeveloper/allconnected/activity_changeresetpassword.java
+++ app/src/main/java/com/epicdeveloper/allconnected/activity_changeresetpassword.java
@@ -6,13 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -50,103 +48,91 @@ public class activity_changeresetpassword extends AppCompatActivity {
         selectedLanguage = MainActivity.userlanguage;
         context = LocaleHelper.setLocale(activity_changeresetpassword.this, selectedLanguage);
         resources = context.getResources();
-        newUserlbl = (TextView) findViewById(R.id.newUserlbl);
-        newPass = (EditText) findViewById(R.id.newPassField);
-        confirmPass = (EditText) findViewById(R.id.newConfirmPass);
-        btnchangePass = (Button) findViewById(R.id.changePassButton);
-        errorText = (TextView) findViewById(R.id.errChangeTextView);
+        newUserlbl = findViewById(R.id.newUserlbl);
+        newPass = findViewById(R.id.newPassField);
+        confirmPass = findViewById(R.id.newConfirmPass);
+        btnchangePass =findViewById(R.id.changePassButton);
+        errorText = findViewById(R.id.errChangeTextView);
         newPass.setEnabled(true);
         confirmPass.setEnabled(true);
+        translatefields();
 
-        translatefields(selectedLanguage);
+        newPass.setOnTouchListener((v, event) -> {
+            //final int DRAWABLE_LEFT = 0;
+            //final int DRAWABLE_TOP = 1;
+            final int DRAWABLE_RIGHT = 2;
+            //final int DRAWABLE_BOTTOM = 3;
 
-        newPass.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int DRAWABLE_LEFT = 0;
-                final int DRAWABLE_TOP = 1;
-                final int DRAWABLE_RIGHT = 2;
-                final int DRAWABLE_BOTTOM = 3;
-
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    if(event.getRawX() >= (newPass.getRight() - newPass.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-                        if (newPass.getTransformationMethod() == HideReturnsTransformationMethod.getInstance()){
-                            newPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                            newPass.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.showpass, 0);
-                        }else{
-                            newPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                            newPass.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.hidepass, 0);
-                        }
-                        return true;
+            if(event.getAction() == MotionEvent.ACTION_UP) {
+                if(event.getRawX() >= (newPass.getRight() - newPass.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    if (newPass.getTransformationMethod() == HideReturnsTransformationMethod.getInstance()){
+                        newPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        newPass.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.showpass, 0);
+                    }else{
+                        newPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                        newPass.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.hidepass, 0);
                     }
+                    return true;
                 }
-
-                return false;
             }
+
+            return false;
         });
 
-        confirmPass.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int DRAWABLE_LEFT = 0;
-                final int DRAWABLE_TOP = 1;
-                final int DRAWABLE_RIGHT = 2;
-                final int DRAWABLE_BOTTOM = 3;
+        confirmPass.setOnTouchListener((v, event) -> {
+            //final int DRAWABLE_LEFT = 0;
+            //final int DRAWABLE_TOP = 1;
+            final int DRAWABLE_RIGHT = 2;
+            //final int DRAWABLE_BOTTOM = 3;
 
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    if(event.getRawX() >= (confirmPass.getRight() - confirmPass.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-                        if (confirmPass.getTransformationMethod() == HideReturnsTransformationMethod.getInstance()){
-                            confirmPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                            confirmPass.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.showpass, 0);
-                        }else{
-                            confirmPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                            confirmPass.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.hidepass, 0);
-                        }
-                        return true;
+            if(event.getAction() == MotionEvent.ACTION_UP) {
+                if(event.getRawX() >= (confirmPass.getRight() - confirmPass.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    if (confirmPass.getTransformationMethod() == HideReturnsTransformationMethod.getInstance()){
+                        confirmPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        confirmPass.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.showpass, 0);
+                    }else{
+                        confirmPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                        confirmPass.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.hidepass, 0);
                     }
+                    return true;
                 }
-
-                return false;
             }
+
+            return false;
         });
 
-        btnchangePass.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                if (btnchangePass.getText().equals(resources.getString(R.string.backLogin))){
-                    finish();
+        btnchangePass.setOnClickListener(v -> {
+            if (btnchangePass.getText().equals(resources.getString(R.string.backLogin))){
+                finish();
+                return;
+            }
+            if (btnchangePass.getText().equals(resources.getString(R.string.changePssButton))) {
+                if (TextUtils.isEmpty(newPass.getText().toString()) || TextUtils.isEmpty(confirmPass.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),resources.getString(R.string.noEmptyPassFields), Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (btnchangePass.getText().equals(resources.getString(R.string.changePssButton))) {
-                    if (TextUtils.isEmpty(newPass.getText().toString()) || TextUtils.isEmpty(confirmPass.getText().toString())) {
-                        Toast.makeText(getApplicationContext(),resources.getString(R.string.noEmptyPassFields), Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    if (!LoginErrorsValidation.isValid(newPass.getText().toString())) {
-                        Toast.makeText(getApplicationContext(),resources.getString(R.string.passStrongErr), Toast.LENGTH_LONG).show();
-                        return;
-                    }
-
-                    if (!TextUtils.equals(newPass.getText().toString(), confirmPass.getText().toString())) {
-                        Toast.makeText(getApplicationContext(),resources.getString(R.string.notPassMatch), Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    changePass(newPass.getText().toString());
-                    Toast.makeText(getApplicationContext(),resources.getString(R.string.passUpdated), Toast.LENGTH_SHORT).show();
-                    btnchangePass.setText(resources.getString(R.string.backLogin));
-                    newPass.setEnabled(false);
-                    confirmPass.setEnabled(false);
+                if (!LoginErrorsValidation.isValid(newPass.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),resources.getString(R.string.passStrongErr), Toast.LENGTH_LONG).show();
+                    return;
                 }
 
+                if (!TextUtils.equals(newPass.getText().toString(), confirmPass.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),resources.getString(R.string.notPassMatch), Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
-        });
+                changePass(newPass.getText().toString());
+                Toast.makeText(getApplicationContext(),resources.getString(R.string.passUpdated), Toast.LENGTH_SHORT).show();
+                btnchangePass.setText(resources.getString(R.string.backLogin));
+                newPass.setEnabled(false);
+                confirmPass.setEnabled(false);
+            }
+
+            });
 
     }
 
-    private void translatefields(String selectedLanguage) {
+    private void translatefields() {
         newUserlbl.setText(resources.getString(R.string.passChangeLbl));
         newPass.setHint(resources.getString(R.string.newPassHint));
         confirmPass.setHint(resources.getString(R.string.confirmNewPassHint));
@@ -156,12 +142,12 @@ public class activity_changeresetpassword extends AppCompatActivity {
 
     private void changePass(final String passchange){
         changePassword = FirebaseDatabase.getInstance().getReference("Users");
-        Query getData = changePassword.orderByChild("plate_user").equalTo(MainActivity.plate_user.toString().toUpperCase());
+        Query getData = changePassword.orderByChild("plate_user").equalTo(MainActivity.plate_user.toUpperCase());
         ValueEventListener valueEventListener = (new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot ds: snapshot.getChildren()){
-                    Map<String, Object> updates = new HashMap<String, Object>();
+                    Map<String, Object> updates = new HashMap<>();
                     updates.put("user_password", passchange);
                     updates.put("resetPass","0");
                     ds.getRef().updateChildren(updates);
