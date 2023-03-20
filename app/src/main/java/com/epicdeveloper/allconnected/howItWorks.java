1ea7cf89a2817318553fc692b3e360e03a379387
franklyn-noyola <franklyn.noyola@gmail.com>
2023-03-20 22:26:43 +0100

    update

--- app/src/main/java/com/epicdeveloper/allconnected/howItWorks.java
+++ app/src/main/java/com/epicdeveloper/allconnected/howItWorks.java
@@ -28,36 +28,36 @@ public class howItWorks extends AppCompatActivity {
         resources = context.getResources();
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0099CC")));
         getSupportActionBar().setTitle(resources.getString(R.string.action_howItWorks));
-        profile = findViewById(R.id.profile);
+        //profile = findViewById(R.id.profile);
         chat = findViewById(R.id.chat);
         sendText = findViewById(R.id.sendText);
         receiveText = findViewById(R.id.receiveText);
-        about = findViewById(R.id.about);
+        //about = findViewById(R.id.about);
         setting = findViewById(R.id.setting);
-        contact = findViewById(R.id.contact);
-        profileText = findViewById(R.id.profileText);
+        //contact = findViewById(R.id.contact);
+        //profileText = findViewById(R.id.profileText);
         chatText = findViewById(R.id.chatText);
         sendTextLbl = findViewById(R.id.sendTextLbl);
         receiveTextLbl = findViewById(R.id.receiveTextLbl);
-        aboutText = findViewById(R.id.aboutText);
+        //aboutText = findViewById(R.id.aboutText);
         settingText = findViewById(R.id.settingText);
-        contactText = findViewById(R.id.contactText);
+        //contactText = findViewById(R.id.contactText);
 
-        profile.setText(resources.getString(R.string.profile_menu));
+        //profile.setText(resources.getString(R.string.profile_menu));
         chat.setText(resources.getString(R.string.chat_menu));
         sendText.setText(resources.getString(R.string.sendNoti));
         receiveText.setText(resources.getString(R.string.receiveNotification));
-        about.setText(resources.getString(R.string.action_about));
+        //about.setText(resources.getString(R.string.action_about));
         setting.setText(resources.getString(R.string.action_settings));
-        contact.setText(resources.getString(R.string.action_contacts));
+        //contact.setText(resources.getString(R.string.action_contacts));
 
-        profileText.setText(resources.getString(R.string.profileText));
+        //profileText.setText(resources.getString(R.string.profileText));
         chatText.setText(resources.getString(R.string.chatText));
         sendTextLbl.setText(resources.getString(R.string.sendNotiText));
         receiveTextLbl.setText(resources.getString(R.string.receiveText));
-        aboutText.setText(resources.getString(R.string.aboutText));
+        //aboutText.setText(resources.getString(R.string.aboutText));
         settingText.setText(resources.getString(R.string.settingText));
-        contactText.setText(resources.getString(R.string.contactText));
+        //contactText.setText(resources.getString(R.string.contactText));
 
     }
 }
