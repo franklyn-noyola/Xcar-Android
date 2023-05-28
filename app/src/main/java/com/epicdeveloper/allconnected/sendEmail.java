27672b820709f11aada0ddcd7ca23315176cf2f2
franklyn-noyola <franklyn.noyola@gmail.com>
2023-05-28 20:38:11 +0200

    updated

--- app/src/main/java/com/epicdeveloper/allconnected/sendEmail.java
+++ app/src/main/java/com/epicdeveloper/allconnected/sendEmail.java
@@ -2,16 +2,14 @@ package com.epicdeveloper.allconnected;
 
 import android.os.StrictMode;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
