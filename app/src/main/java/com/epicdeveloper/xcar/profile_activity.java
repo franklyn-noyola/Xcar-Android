121d7c9e811f366b8b62daed702262c95be256c8
franklyn-noyola <franklyn.noyola@gmail.com>
2023-08-30 03:52:32 +0200

    XCar updated

--- app/src/main/java/com/epicdeveloper/xcar/profile_activity.java
+++ app/src/main/java/com/epicdeveloper/xcar/profile_activity.java
@@ -172,7 +172,7 @@ public class profile_activity extends AppCompatActivity {
                 startActivity(intent);
                 return true;
             case shareAction:
-                String shareLink = "https://play.google.com/store/apps/details?id=com.epicdeveloper.XCar";
+                String shareLink = "https://play.google.com/store/apps/details?id=com.epicdeveloper.allconnected";
                 intent = new Intent();
                 intent.setAction(Intent.ACTION_SEND);
                 intent.putExtra(Intent.EXTRA_TEXT, shareLink);
