7af7d236b878351138be462f9f29b78afa0a49d0
franklyn-noyola <franklyn.noyola@gmail.com>
2024-02-19 17:54:44 +0100

    updated

--- app/src/main/java/com/epicdeveloper/allconnected/ui/Perfil/fragment_profile.java
+++ app/src/main/java/com/epicdeveloper/allconnected/ui/Perfil/fragment_profile.java
@@ -1,31 +1,28 @@
-package com.epicdeveloper.allconnected.ui.Perfil;
+    package com.epicdeveloper.xcar.ui.Perfil;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 
 import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.PopupWindow;
@@ -34,12 +31,14 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.epicdeveloper.allconnected.LocaleHelper;
-import com.epicdeveloper.allconnected.MainActivity;
-import com.epicdeveloper.allconnected.R;
-import com.epicdeveloper.allconnected.profile_activity;
-import com.epicdeveloper.allconnected.ui.LoginErrorsValidation;
+import com.epicdeveloper.xcar.LocaleHelper;
+import com.epicdeveloper.xcar.MainActivity;
+import com.epicdeveloper.xcar.R;
+import com.epicdeveloper.xcar.additionalPlate;
+import com.epicdeveloper.xcar.contacto;
+import com.epicdeveloper.xcar.ui.LoginErrorsValidation;
 
+import com.epicdeveloper.xcar.ui.home.fragment_home;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.firebase.database.DataSnapshot;
@@ -54,24 +53,48 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
-import static com.epicdeveloper.allconnected.R.layout.changepass;
+import static com.epicdeveloper.xcar.MainActivity.chatScreen;
+import static com.epicdeveloper.xcar.MainActivity.email_user;
+import static com.epicdeveloper.xcar.MainActivity.getSelectedPlate;
+import static com.epicdeveloper.xcar.R.layout.changepass;
+import static com.epicdeveloper.xcar.R.layout.fragment_home;
 
-public class fragment_profile extends Fragment {
-    TextView userSelected, plateSelected,  emailSelected, infoLbl;
+    public class fragment_profile extends Fragment {
+    TextView userSelected, plateSelected, emailSelected, infoLbl;
     String selectedLanguage;
+
+    ArrayAdapter<String> playlistAdapter;
+    Button deletePlate;
+
+    Button addNewPlate;
+
+    String plate_0;
+    String plate_1;
+
+    String plate_2;
+
+    int position;
+
+    String list_plate;
+
+    int platesCount = 0;
+
+    public List<String>  listPlate;
     EditText brandCarField, modelCarField, colorCarField, yearCarField, newPassField, confirmPassField;
     public List<String> cartypeSelected;
     Spinner carTypeField;
+    Spinner addedPlates;
     TextView carTypeFieldLbl;
     Button btnModiInfoButton, btnchangePassButton, btnCancelPassButton, btnUpdatePass, btnCancelModInfo;
-    public  Object typeProfileCarUser;
-    public  Object brandProfileCarUser;
-    public  Object modelProfileCaruser;
-    public  Object colorProfileCarUser;
-    public  Object yearProfileCarUser;
+    public Object typeProfileCarUser;
+    public Object brandProfileCarUser;
+    public Object modelProfileCaruser;
+    public Object colorProfileCarUser;
+    public Object yearProfileCarUser;
     public View Profile;
+
+    public String selectedPlate;
     Context context;
     Resources resources;
     View view;
@@ -90,8 +113,10 @@ public class fragment_profile extends Fragment {
         Profile = inflater.inflate(R.layout.fragment_profile_fragment, container, false);
         inflaterView = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         adview = Profile.findViewById(R.id.adView);
+        addNewPlate = Profile.findViewById(R.id.addnewplatebcn);
         AdRequest adRequest = new AdRequest.Builder().build();
         adview.loadAd(adRequest);
+        deletePlate = Profile.findViewById(R.id.deleteplate);
         cartypeSelected = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.Vehiculos)));
         infoLbl = Profile.findViewById(R.id.infoLbl);
         if (selectedLanguage.equals("RU")) {
@@ -102,6 +127,7 @@ public class fragment_profile extends Fragment {
         userSelected = Profile.findViewById(R.id.userDataLbl);
         plateSelected = Profile.findViewById(R.id.plateDataLbl);
         emailSelected = Profile.findViewById(R.id.emailDataLbl);
+        addedPlates = Profile.findViewById(R.id.plate);
         carTypeField = Profile.findViewById(R.id.vehicleType);
         carTypeField.setEnabled(false);
         carTypeField.setVisibility(View.INVISIBLE);
@@ -116,14 +142,48 @@ public class fragment_profile extends Fragment {
         colorCarField.setHint(resources.getString(R.string.colorHint));
         yearCarField = Profile.findViewById(R.id.carYear);
         yearCarField.setHint(resources.getString(R.string.yearHint));
+        deletePlate.setHint(R.string.delete);
         btnModiInfoButton = Profile.findViewById(R.id.ModInfo);
         btnModiInfoButton.setHint(resources.getString(R.string.modifyInfo));
         btnchangePassButton = Profile.findViewById(R.id.modPass);
         btnchangePassButton.setHint(resources.getString(R.string.changePassHint));
         userSelected.setText(MainActivity.UserSel);
-        plateSelected.setText(MainActivity.userSelected);
         emailSelected.setText(MainActivity.emailSelected);
-        getInfoData();
+        getPlateList();
+
+        if (TextUtils.isEmpty(getSelectedPlate)) {
+            userSelected.setText(MainActivity.UserSel);
+            plateSelected.setText(MainActivity.plate_user);
+            emailSelected.setText(MainActivity.emailSelected);
+            getInfoData();
+
+       }else {
+            getSelectedPlate(getSelectedPlate);
+            plateSelected.setText(getSelectedPlate);
+        }
+
+        addedPlates.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                MainActivity.getSelectedPlate = addedPlates.getSelectedItem().toString();
+                MainActivity.setSelection = addedPlates.getSelectedItemPosition();
+                getSelectedPlate(getSelectedPlate);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        addNewPlate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), additionalPlate.class);
+                startActivity(intent);
+            }
+        });
 
         btnModiInfoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -136,6 +196,32 @@ public class fragment_profile extends Fragment {
             @Override
             public void onClick(View v) {
                 changePassword();
+            }
+        });
+
+        deletePlate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position = addedPlates.getSelectedItemPosition();
+                if (position == 0) {
+                    plate_0 = addedPlates.getSelectedItem().toString();
+                    plate_1 = addedPlates.getItemAtPosition(1).toString();
+                }else {
+                    plate_2 = addedPlates.getItemAtPosition(addedPlates.getSelectedItemPosition()).toString();;
+                }
+                AlertDialog alert = new AlertDialog.Builder(getActivity()).create();
+                alert.setMessage(resources.getString(R.string.deletePlateMsg));
+                alert.setButton(AlertDialog.BUTTON_POSITIVE,resources.getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deletePlateSelected();
+                        Toast.makeText(getActivity(), resources.getString(R.string.plateDeleted), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setButton(AlertDialog.BUTTON_NEGATIVE,resources.getString(R.string.No), (DialogInterface.OnClickListener) null);
+                alert.show();
+
+
             }
         });
 
@@ -157,6 +243,108 @@ public class fragment_profile extends Fragment {
         });
         return Profile;
     }
+
+
+        private void deletePlateSelected(){
+            String plateToDelete;
+            if (position == 0){
+                plateToDelete = plate_0;
+            } else{
+                plateToDelete = plate_2;
+            }
+
+            String dot1 = new String (email_user);
+            String dot2 = dot1.replace(".","_");
+            DatabaseReference Chat = FirebaseDatabase.getInstance().getReference("Users/"+dot2);
+            Chat.orderByChild("plate_user").equalTo(plateToDelete).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String key = null;
+                    for (DataSnapshot ds: snapshot.getChildren()){
+                        key = ds.getKey();
+                    }
+                    snapshot.child(key).getRef().removeValue();
+                    if (position==0){
+                        updateNextPlate(plate_1);
+                        MainActivity.setSelection = 0;
+                        playlistAdapter.remove((String)addedPlates.getSelectedItem());
+                        playlistAdapter.notifyDataSetChanged();
+                    }else{
+                        MainActivity.setSelection = 0;
+                        playlistAdapter.remove((String)addedPlates.getSelectedItem());
+                        playlistAdapter.notifyDataSetChanged();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+        public void updateNextPlate(String plate){
+            String dot1 = new String (MainActivity.emailSelected);
+            String dot2 = dot1.replace(".","_");
+            Users = FirebaseDatabase.getInstance().getReference("Users/"+dot2);
+            Users.orderByChild("plate_user").equalTo(plate).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot ds: snapshot.getChildren()){
+                        Map<String, Object> updates = new HashMap<String, Object>();
+                        updates.put("type", "M");
+                        ds.getRef().updateChildren(updates);
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+
+        }
+
+
+    public void getPlateList() {
+        String dot1 = new String(email_user);
+        String dot2 = dot1.replace(".", "_");
+        DatabaseReference plateList = FirebaseDatabase.getInstance().getReference("Users/" + dot2);
+        listPlate = new ArrayList<String>();
+
+        plateList.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listPlate.clear();
+                for (DataSnapshot plateList : snapshot.getChildren()){
+                    list_plate   = plateList.child("plate_user").getValue().toString();
+                    listPlate.add(list_plate);
+                }
+                playlistAdapter = new ArrayAdapter<String>(getContext(), R.layout.support_simple_spinner_dropdown_item, listPlate);
+                addedPlates.setAdapter(playlistAdapter);
+                if (TextUtils.isEmpty(getSelectedPlate)){
+                    addedPlates.setSelection(0);
+                }else{
+                    addedPlates.setSelection(MainActivity.setSelection);
+                }
+                platesCount = listPlate.size();
+                if ( platesCount > 1){
+                    deletePlate.setVisibility(View.VISIBLE);
+                }else {
+                    deletePlate.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+
 
     @SuppressLint("ClickableViewAccessibility")
     private void changePassword() {
@@ -252,8 +440,10 @@ public class fragment_profile extends Fragment {
                             Toast.makeText(context, resources.getString(R.string.noPassMatch), Toast.LENGTH_SHORT).show();
                             return;
                         }
-                        Users = FirebaseDatabase.getInstance().getReference("Users");
-                        Query getData = Users.orderByChild("plate_user").equalTo(MainActivity.plateUser.toUpperCase());
+                        String dot1 = new String (email_user);
+                        String dot2 = dot1.replace(".","_");
+                        Users = FirebaseDatabase.getInstance().getReference("Users/"+dot2);
+                        Query getData = Users.orderByChild("user_email").equalTo(MainActivity.emailSelected);
                         final String changPass = newPassField.getText().toString();
                         ValueEventListener valueEventListener = (new ValueEventListener() {
                             @Override
@@ -284,6 +474,10 @@ public class fragment_profile extends Fragment {
     }
 
     private void updateDataInfo() {
+        if (TextUtils.isEmpty(getSelectedPlate)){
+            getSelectedPlate = MainActivity.plate_user;
+        }
+
         if (btnModiInfoButton.getHint().equals(resources.getString(R.string.modifyInfo))){
             btnModiInfoButton.setHint(resources.getString(R.string.updateInfo));
 
@@ -302,9 +496,11 @@ public class fragment_profile extends Fragment {
 
        if (btnModiInfoButton.getHint().equals(resources.getString(R.string.updateInfo))){
             btnModiInfoButton.setHint(resources.getString(R.string.modifyInfo));
-
-           Users = FirebaseDatabase.getInstance().getReference("Users");
-           Users.orderByChild("plate_user").equalTo(MainActivity.plateUser.toUpperCase()).addListenerForSingleValueEvent(new ValueEventListener() {
+           String dot1 = new String (MainActivity.emailSelected);
+           String dot2 = dot1.replace(".","_");
+           Users = FirebaseDatabase.getInstance().getReference("Users/"+dot2);
+                ((Spinner)carTypeField).getSelectedView().setEnabled(false);
+           Users.orderByChild("plate_user").equalTo(getSelectedPlate).addListenerForSingleValueEvent(new ValueEventListener() {
                @Override
                public void onDataChange(@NonNull DataSnapshot snapshot) {
                    for (DataSnapshot ds: snapshot.getChildren()){
@@ -322,8 +518,7 @@ public class fragment_profile extends Fragment {
 
                }
            });
-                ((Spinner)carTypeField).getSelectedView().setEnabled(false);
-                if (carTypeField.getSelectedItemPosition() == 0) {
+           if (carTypeField.getSelectedItemPosition() == 0) {
                     carTypeFieldLbl.setText("");
                 }else {
                     carTypeFieldLbl.setText(carTypeField.getSelectedItem().toString());
@@ -337,14 +532,47 @@ public class fragment_profile extends Fragment {
                 modelCarField.setEnabled(false);
                 colorCarField.setEnabled(false);
                 yearCarField.setEnabled(false);
+                btnCancelModInfo.setVisibility(View.INVISIBLE);
                 Toast.makeText(context, resources.getString(R.string.infoUpdated), Toast.LENGTH_SHORT).show();
                 btnchangePassButton.setEnabled(true);
         }
     }
 
+    private void getSelectedPlate(String plate) {
+        String dot1 = new String (email_user);
+        String dot2 = dot1.replace(".","_");
+        Users = FirebaseDatabase.getInstance().getReference("Users/"+dot2);
+        Users.orderByChild("plate_user").equalTo(plate).addListenerForSingleValueEvent(new ValueEventListener() {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        typeProfileCarUser = snapshot.child("cartype").getValue();
+                        brandProfileCarUser = snapshot.child("carbrand").getValue();
+                        modelProfileCaruser = snapshot.child("carmodel").getValue();
+                        colorProfileCarUser = snapshot.child("carcolor").getValue();
+                        yearProfileCarUser = snapshot.child("year").getValue();
+                    }
+                    carTypeFieldLbl.setText(typeProfileCarUser.toString());
+                    brandCarField.setText(brandProfileCarUser.toString());
+                    modelCarField.setText(modelProfileCaruser.toString());
+                    colorCarField.setText(colorProfileCarUser.toString());
+                    yearCarField.setText(yearProfileCarUser.toString());
+                    plateSelected.setText(plate);
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     private void getInfoData() {
-        Users = FirebaseDatabase.getInstance().getReference("Users");
-        Users.orderByChild("plate_user").equalTo(MainActivity.plateUser.toUpperCase()).addListenerForSingleValueEvent(new ValueEventListener() {
+        String dot1 = new String (email_user);
+        String dot2 = dot1.replace(".","_");
+        Users = FirebaseDatabase.getInstance().getReference("Users/"+dot2);
+        Users.orderByChild("type").equalTo("M").addListenerForSingleValueEvent(new ValueEventListener() {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
